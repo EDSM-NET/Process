5daf3d883d56b52404c2504a32f44a9fed0b30d5
AnthorNet <anthor.net@gmail.com>
2019-03-05 14:02:17 +0100

    Update bodies dump generation

--- Dump/Body.php
+++ Dump/Body.php
@@ -12,112 +12,133 @@ class Body extends Process
 {
     static private $tempFile        = APPLICATION_PATH . '/Data/Temp/bodies.json';
     static private $finalFile       = PUBLIC_PATH . '/dump/bodies.json';
-    
+
+    static private $limit           = 250000;
+
     static public function run()
     {
         static::log('Generating ' . str_replace(PUBLIC_PATH, '', static::$finalFile));
-    
+
         if(file_exists(static::$tempFile))
         {
             unlink(static::$tempFile);
         }
-        
-        $systemsModel       = new \Models_Systems;
-        $systemsHidesModel  = new \Models_Systems_Hides;
-        $systemsBodiesModel = new \Models_Systems_Bodies;
-        
+
+        $systemsModel               = new \Models_Systems;
+        $systemsHidesModel          = new \Models_Systems_Hides;
+
+        $systemsBodiesModel         = new \Models_Systems_Bodies;
+        $systemsBodiesOrbitalModel  = new \Models_Systems_Bodies_Orbital;
+        $systemsBodiesSurfaceModel  = new \Models_Systems_Bodies_Surface;
+        $systemsBodiesParentsModel  = new \Models_Systems_Bodies_Parents;
+
         // Disable cache
         $systemsBodiesModel->disableCache();
-        
-        // Fill an array with all days from start to yesterday
-        $startDate  = strtotime('2015-05-12');
-        $endDate    = strtotime('yesterday');
-        
-        $loopDates  = array();
-        
-        while($startDate < $endDate)
+
+        // Get current max id
+        $maxId  = $systemsBodiesModel->fetchRow(
+            $systemsBodiesModel->select()->order('id DESC')->limit(1)
+        );
+        $maxId  = $maxId->id;
+
+        // Generate hidden system array
+        $hiddenSystems      = array();
+        $tmpHiddenSystems   = $systemsHidesModel->fetchAll();
+
+        foreach($tmpHiddenSystems AS $hiddenSystem)
         {
-            $loopDates[]    = date('Y-m-d', $startDate);
-            $startDate     += 86400; // Add 1 day
-        }
-        
-        file_put_contents(static::$tempFile, '[' . PHP_EOL);
-        $line = 0;
-        
-        foreach($loopDates AS $key => $date)
-        {
-            static::log('    - ' . \Zend_Locale_Format::toNumber($key / count($loopDates) * 100, array('precision' => 2)) . '%');
-            
-            $select     = $systemsModel->select()
-                                       ->from($systemsModel, array('id', 'id64', 'name'))
-                                       ->where('updatetime > ?', $date . ' 00:00:00')
-                                       ->where('updatetime <= ?', date('Y-m-d', (strtotime($date) + 86400)) . ' 00:00:00')
-                                       ->where(
-                                           'id NOT IN(?)',
-                                           new \Zend_Db_Expr($systemsHidesModel->select()->from($systemsHidesModel, array('refSystem')))
-                                       )
-                                       ->order('updatetime ASC');
-            
-            $systems    = $systemsModel->fetchAll($select);
-            
-            if(!is_null($systems))
+            if(!in_array($hiddenSystem->refSystem, $hiddenSystems))
             {
-                $systems = $systems->toArray();
-                
-                foreach($systems AS $system)
+                $hiddenSystems[] = $hiddenSystem->refSystem;
+            }
+        }
+
+        unset($tmpHiddenSystems);
+
+        // Start temp file
+        file_put_contents(static::$tempFile, '[' . PHP_EOL);
+        $line       = 0;
+        $currentId  = 0;
+
+        while($currentId < $maxId)
+        {
+            static::log('    - ' . \Zend_Locale_Format::toNumber($currentId / $maxId * 100, array('precision' => 2)) . '% (Current ID: ' . $currentId . ')');
+
+            $select     = $systemsBodiesModel->select()
+                                             ->setIntegrityCheck(false)
+                                             ->from(
+                                                 $systemsBodiesModel,
+                                                 array(
+                                                     $systemsBodiesModel->info('name') . '.*',
+                                                     $systemsBodiesOrbitalModel->info('name') . '.*',
+                                                     $systemsBodiesSurfaceModel->info('name') . '.*',
+                                                     $systemsBodiesParentsModel->info('name') . '.*',
+                                                     'systemId64'   => $systemsModel->info('name') . '.id64',
+                                                     'systemName'   => $systemsModel->info('name') . '.name',
+                                                 )
+                                             )
+                                             ->joinInner($systemsModel->info('name'), $systemsBodiesModel->info('name') . '.refSystem = ' . $systemsModel->info('name') . '.id', null)
+                                             ->joinLeft($systemsBodiesOrbitalModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesOrbitalModel->info('name') . '.refBody')
+                                             ->joinLeft($systemsBodiesSurfaceModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesSurfaceModel->info('name') . '.refBody')
+                                             ->joinLeft($systemsBodiesParentsModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesParentsModel->info('name') . '.refBody')
+                                             ->where($systemsBodiesModel->info('name') . '.id > ?', $currentId)
+                                             ->where($systemsBodiesModel->info('name') . '.id <= ?', $maxId)
+                                             ->where($systemsBodiesModel->info('name') . '.refSystem NOT IN(?)', $hiddenSystems)
+                                             ->order($systemsBodiesModel->info('name') . '.id ASC')
+                                             ->limit(static::$limit);
+
+            $bodies     = $systemsBodiesModel->fetchAll($select);
+
+            if(!is_null($bodies))
+            {
+                $bodies = $bodies->toArray();
+
+                foreach($bodies AS $body)
                 {
-                    $select = $systemsBodiesModel->select()
-                                                 ->where('refSystem = ?', $system['id']);
-                    
-                    $bodies = $systemsBodiesModel->fetchAll($select);
-                    
-                    if(!is_null($bodies))
+                    $currentId  = $body['id'];
+                    $refSystem  = $body['refSystem'];
+                    $systemId64 = $body['systemId64'];
+                    $systemName = $body['systemName'];
+
+                    $body       = \EDSM_System_Body::getInstance($body['id'], $body);
+                    $tmpBody    = $body->renderApiArray(true);
+
+                    $tmpBody['systemId']    = (int) $refSystem;
+                    $tmpBody['systemId64']  = (!is_null($systemId64)) ? (int) $systemId64 : null;;
+                    $tmpBody['systemName']  = $systemName;
+
+                    //\Zend_Debug::dump($tmpBody);
+                    //exit();
+
+                    if($line > 0)
                     {
-                        $bodies = $bodies->toArray();
-                        
-                        foreach($bodies AS $body)
-                        {
-                            $body       = \EDSM_System_Body::getInstance($body['id'], $body);
-                            $tmpBody    = $body->renderApiArray(true);
-                            
-                            $tmpBody['systemId']    = (int) $system['id'];
-                            $tmpBody['systemId64']  = (!is_null($system['id64'])) ? (int) $system['id64'] : null;;
-                            $tmpBody['systemName']  = $system['name'];
-                            
-                            //\Zend_Debug::dump($tmpBody);
-                            //exit();
-                            
-                            if($line > 0)
-                            {
-                                file_put_contents(static::$tempFile, ',' . PHP_EOL, FILE_APPEND);
-                            }
-                            
-                            file_put_contents(static::$tempFile, '    ' . \Zend_Json::encode($tmpBody), FILE_APPEND);
-                            
-                            $line++;
-                        }
+                        file_put_contents(static::$tempFile, ',' . PHP_EOL, FILE_APPEND);
                     }
+
+                    file_put_contents(static::$tempFile, '    ' . \Zend_Json::encode($tmpBody), FILE_APPEND);
+
+                    $line++;
                 }
             }
         }
-        
+
         file_put_contents(static::$tempFile, PHP_EOL . ']', FILE_APPEND);
-        
+
         rename(static::$tempFile, static::$finalFile);
-        
+
         // Add ContentLength JSON
         file_put_contents(
             str_replace('.json', '.length.json', static::$finalFile),
             filesize(static::$finalFile)
         );
-        
+
         $systemsModel->getAdapter()->closeConnection();
-        
+
         // Enable cache
         $systemsBodiesModel->enableCache();
-        
+
         unset($systemsModel, $systemsHidesModel, $systemsBodiesModel, $systems);
-        
+
         static::endLog();
         return;
     }
--- Dump/BodyLastWeek.php
+++ Dump/BodyLastWeek.php
@@ -12,84 +12,160 @@ class BodyLastWeek extends Process
 {
     static private $tempFile        = APPLICATION_PATH . '/Data/Temp/bodies7days.json';
     static private $finalFile       = PUBLIC_PATH . '/dump/bodies7days.json';
-    
-    static private $countPerPages   = 25000;
-    
+
+    static private $limit           = 100000;
+
     static public function run()
     {
         static::log('Generating ' . str_replace(PUBLIC_PATH, '', static::$finalFile));
-    
+
         if(file_exists(static::$tempFile))
         {
             unlink(static::$tempFile);
         }
-        
-        $systemsBodiesModel = new \Models_Systems_Bodies;
-        
-        file_put_contents(static::$tempFile, '[' . PHP_EOL);
-        
-        $select     = $systemsBodiesModel->select()
-                                         ->where('dateUpdated >= DATE(DATE_SUB(NOW(), INTERVAL 7 DAY))')
-                                         ->where('dateUpdated <= ?', date('Y-m-d', strtotime('yesterday')) . ' 23:59:59')
-                                         ->order('dateUpdated ASC');
-        
-        $adapter    = new \Zend_Paginator_Adapter_DbTableSelect($select);
-        $paginator  = new \Zend_Paginator($adapter);
-        $paginator->setItemCountPerPage( static::$countPerPages );
-        
-        $pageCount  = $paginator->count();
-        $key        = 0;
-        
-        for($i = 1; $i <= $pageCount; $i++)
+
+        $systemsModel               = new \Models_Systems;
+        $systemsHidesModel          = new \Models_Systems_Hides;
+
+        $systemsBodiesModel         = new \Models_Systems_Bodies;
+        $systemsBodiesOrbitalModel  = new \Models_Systems_Bodies_Orbital;
+        $systemsBodiesSurfaceModel  = new \Models_Systems_Bodies_Surface;
+        $systemsBodiesParentsModel  = new \Models_Systems_Bodies_Parents;
+
+        // Disable cache
+        $systemsBodiesModel->disableCache();
+
+        // Get current max id
+        if(defined('CRON_BODY_MAXID'))
         {
-            static::log('    - ' . \Zend_Locale_Format::toNumber($i / $pageCount * 100, array('precision' => 2)) . '%');
-            
-            $paginator->setCurrentPageNumber( $i );
-            
-            $bodies = $paginator->getCurrentItems()->toArray();
-            
-            foreach($bodies AS $body)
+            $maxIdNow  = $maxId = CRON_BODY_MAXID;
+        }
+        else
+        {
+            $maxId  = $systemsBodiesModel->fetchRow(
+                $systemsBodiesModel->select()->order('id DESC')->limit(1)
+            );
+            $maxIdNow  = $maxId = $maxId->id;
+        }
+
+        // Store last maxId
+        $yesterdayKey                   = date('Y-m-d', strtotime('yesterday'));
+        $fileMaxIds                     = APPLICATION_PATH . '/Data/Cache/BodyLastWeekId.json';
+        $arrayMaxIds                    = \Zend_Json::decode(file_get_contents($fileMaxIds));
+
+        if(!array_key_exists($yesterdayKey, $arrayMaxIds))
+        {
+            $arrayMaxIds[$yesterdayKey]     = $maxIdNow;
+            reset($arrayMaxIds);
+        }
+
+        // Get first array maxId
+        $currentId  = $arrayMaxIds[key($arrayMaxIds)];
+
+        // Remove extra day
+        if(count($arrayMaxIds) > 7)
+        {
+            unset($arrayMaxIds[key($arrayMaxIds)]);
+        }
+
+        // Save
+        file_put_contents($fileMaxIds, \Zend_Json::encode($arrayMaxIds));
+
+        // Generate hidden system array
+        $hiddenSystems      = array();
+        $tmpHiddenSystems   = $systemsHidesModel->fetchAll();
+
+        foreach($tmpHiddenSystems AS $hiddenSystem)
+        {
+            if(!in_array($hiddenSystem->refSystem, $hiddenSystems))
             {
-                $body       = \EDSM_System_Body::getInstance($body['id'], $body);
-                $system     = $body->getSystem();
-                
-                if($system->isHidden() === false)
+                $hiddenSystems[] = $hiddenSystem->refSystem;
+            }
+        }
+
+        unset($tmpHiddenSystems);
+
+        // Start temp file
+        file_put_contents(static::$tempFile, '[' . PHP_EOL);
+        $line       = 0;
+        $total      = $maxIdNow - $currentId;
+
+        while($currentId < $maxIdNow)
+        {
+            static::log('    - ' . \Zend_Locale_Format::toNumber( ($total - ($maxIdNow - $currentId)) / $total * 100, array('precision' => 2)) . '% (Current ID: ' . $currentId . ')');
+
+            $select     = $systemsBodiesModel->select()
+                                             ->setIntegrityCheck(false)
+                                             ->from(
+                                                 $systemsBodiesModel,
+                                                 array(
+                                                     $systemsBodiesModel->info('name') . '.*',
+                                                     $systemsBodiesOrbitalModel->info('name') . '.*',
+                                                     $systemsBodiesSurfaceModel->info('name') . '.*',
+                                                     $systemsBodiesParentsModel->info('name') . '.*',
+                                                     'systemId64'   => $systemsModel->info('name') . '.id64',
+                                                     'systemName'   => $systemsModel->info('name') . '.name',
+                                                 )
+                                             )
+                                             ->joinInner($systemsModel->info('name'), $systemsBodiesModel->info('name') . '.refSystem = ' . $systemsModel->info('name') . '.id', null)
+                                             ->joinLeft($systemsBodiesOrbitalModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesOrbitalModel->info('name') . '.refBody')
+                                             ->joinLeft($systemsBodiesSurfaceModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesSurfaceModel->info('name') . '.refBody')
+                                             ->joinLeft($systemsBodiesParentsModel->info('name'), $systemsBodiesModel->info('name') . '.id = ' . $systemsBodiesParentsModel->info('name') . '.refBody')
+                                             ->where($systemsBodiesModel->info('name') . '.id > ?', $currentId)
+                                             ->where($systemsBodiesModel->info('name') . '.id <= ?', $maxIdNow)
+                                             ->where($systemsBodiesModel->info('name') . '.refSystem NOT IN(?)', $hiddenSystems)
+                                             ->order($systemsBodiesModel->info('name') . '.id ASC')
+                                             ->limit(static::$limit);
+
+            $bodies     = $systemsBodiesModel->fetchAll($select);
+
+            if(!is_null($bodies))
+            {
+                $bodies = $bodies->toArray();
+
+                foreach($bodies AS $body)
                 {
+                    $currentId  = $body['id'];
+                    $refSystem  = $body['refSystem'];
+                    $systemId64 = $body['systemId64'];
+                    $systemName = $body['systemName'];
+
+                    $body       = \EDSM_System_Body::getInstance($body['id'], $body);
                     $tmpBody    = $body->renderApiArray(true);
-                    
-                    $tmpBody['systemId']    = $system->getId();
-                    $tmpBody['systemId64']  = $system->getId64();
-                    $tmpBody['systemName']  = $system->getName();
-                    
+
+                    $tmpBody['systemId']    = (int) $refSystem;
+                    $tmpBody['systemId64']  = (!is_null($systemId64)) ? (int) $systemId64 : null;;
+                    $tmpBody['systemName']  = $systemName;
+
                     //\Zend_Debug::dump($tmpBody);
                     //exit();
-                    
-                    if($key > 0)
+
+                    if($line > 0)
                     {
                         file_put_contents(static::$tempFile, ',' . PHP_EOL, FILE_APPEND);
                     }
-                    
+
                     file_put_contents(static::$tempFile, '    ' . \Zend_Json::encode($tmpBody), FILE_APPEND);
-                    
-                    $key++;
+
+                    $line++;
                 }
             }
         }
-        
+
         file_put_contents(static::$tempFile, PHP_EOL . ']', FILE_APPEND);
-        
+
         rename(static::$tempFile, static::$finalFile);
-        
+
         // Add ContentLength JSON
         file_put_contents(
             str_replace('.json', '.length.json', static::$finalFile),
             filesize(static::$finalFile)
         );
-        
+
         $systemsBodiesModel->getAdapter()->closeConnection();
-        
+
         unset($systemsBodiesModel, $bodies);
-        
+
         static::endLog();
         return;
     }
